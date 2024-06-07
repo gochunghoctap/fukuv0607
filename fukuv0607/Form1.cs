37bc1e2465b085540f5bc517152f40b90bbcd3d4
katann-IWATATE-tann <104120192+katann-tann@users.noreply.github.com>
2024-06-07 14:05:31 +0900

    fukuv0607

--- fukuv0607/Form1.cs
+++ fukuv0607/Form1.cs
@@ -2,9 +2,41 @@ namespace fukuv0607
 {
     public partial class Form1 : Form
     {
+        int vx = -4;
+        int vy = -4;
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            label1.Text = "ファン　クォック　フン";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left < 0)
+            {
+                vx = -vx;
+            }
+            if (label1.Top < 0)
+            {
+                vy = -vy;
+            }
+            if (label1.Left > 358)
+            {
+                vx = -vx;
+            }
+            if (label1.Left > 278)
+            {
+                vy = -vy;
+            }
         }
     }
 }
